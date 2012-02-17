6f620730f4668870ebfbc706bd5e9d3865cc30d3
Dan Burton <danburton.email@gmail.com>
2012-02-16 23:55:35 -0700

    Change BJ server to use StateT nicely

--- Network/NetSpec/Examples/BlackjackServer.hs
+++ Network/NetSpec/Examples/BlackjackServer.hs
@@ -11,41 +11,50 @@ import Network.NetSpec.Json
 shuffle :: [a] -> IO [a]
 shuffle = return -- TODO
 
-newGame :: IO BlackjackState
-newGame = BJ [[],[]] <$> shuffle fullDeck
+newGame :: BlackjackState
+newGame = BJ [[],[]] fullDeck
   where fullDeck = [Card s v | s <- [minBound .. maxBound]
                              , v <- [minBound .. maxBound]]
 
-deal :: Int -> State BlackjackState ()
-deal 0 = state $ \(BJ [h1,h2] (c:cs)) -> ((), BJ [c:h1, h2] cs)
-deal 1 = state $ \(BJ [h1,h2] (c:cs)) -> ((), BJ [h1, c:h2] cs)
-deal _ = state $ \s -> ((), s)
+stateT :: Monad m => (s -> (a, s)) -> StateT s m a
+stateT = StateT . fmap return
+
+deal :: Monad m => Int -> StateT BlackjackState m ()
+deal 0 = stateT $ \(BJ [h1,h2] (c:cs)) -> ((), BJ [c:h1, h2] cs)
+deal 1 = stateT $ \(BJ [h1,h2] (c:cs)) -> ((), BJ [h1, c:h2] cs)
+deal _ = stateT $ \s -> ((), s)
 
 bust :: BlackjackState -> Bool
 bust _ = True -- TODO
 
+stopIf :: Monad m => (s -> Bool) -> m s -> m (SpecState s)
+stopIf f ms = do
+  s <- ms
+  if f s then stop s else continue s
+
+(.:) :: (a -> b) -> (c -> d -> a) -> c -> d -> b
+(.:) f g x y = f (g x y)
+
+isHit :: BlackjackClientMessage -> Bool
+isHit Hit = True
+isHit _ = False
 
 bjSpec :: NetSpec [] BlackjackState
 bjSpec = ServerSpec {
     _ports = map PortNumber [5001, 5002]
-  , _begin = \[h0, h1] -> do
+  , _begin = \[h0, h1] -> flip execStateT newGame $ do
        h0 ! YouAre 0
        h1 ! YouAre 1
-       s <- newGame
-       let s' = flip execState s $ replicateM_ 2 (deal 0 >> deal 1)
-       return s'
-  , _loop = \[h0,h1] s -> do
-       h0 ! YourTurn (hands s !! 0)
-       h1 ! YourTurn (hands s !! 1)
-       Just choice0 <- receive h0
-       Just choice1 <- receive h1
-       let s' = case choice0 of
-             Hit -> execState (deal 0) s
-             Stand -> s
-           s'' = case choice1 of
-             Hit -> execState (deal 1) s'
-             Stand -> s'
-       if bust s'' then stop s'' else continue s''
+       s <- get
+       deck' <- liftIO $ shuffle $ deck s
+       put $ s { deck = deck' }
+       replicateM_ 2 (deal 0 >> deal 1)
+  , _loop = \hs -> stopIf bust .: execStateT $
+       forM_ (zip hs [0..]) $ \(h, n) -> do
+         curHand <- ((!! n) . hands) <$> get
+         h ! YourTurn curHand
+         Just choice <- receive h
+         when (isHit choice) (deal n)
   , _end = \[h0, h1] s -> do
        h0 ! YouWin s
        h1 ! YouLose s
