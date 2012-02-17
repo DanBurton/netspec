9d1e532ed4eaf9d88e3e81a0f4642b65940cc130
Dan Burton <danburton.email@gmail.com>
2012-02-16 17:09:44 -0700

    eta reduce per hlint suggestion. Nice to show various ways to use bang

--- Network/NetSpec/Examples/Echo.hs
+++ Network/NetSpec/Examples/Echo.hs
@@ -8,7 +8,7 @@ import Network.NetSpec.Text
 main :: IO ()
 main = runSpec ServerSpec
   { _ports = [PortNumber 5001]
-  , _begin = \h -> h ! "I'll echo anything you say."
+  , _begin = (! "I'll echo anything you say.")
   , _loop = \[h] () -> receive h >>= \line -> case line of
        "bye" -> stop_
        _     -> h ! line >> continue_
