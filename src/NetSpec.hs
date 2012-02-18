6397ef721a97316d26bc7004ae411f45c487ad3d
Dan Burton <danburton.email@gmail.com>
2012-02-18 16:52:07 -0700

    Cabalized. Examples currently broken, readme needs to be updated.

--- src/NetSpec.hs
+++ src/NetSpec.hs
@@ -1,3 +1,4 @@
+-- | Simplify static Networking tasks.
 module NetSpec (
     NetSpec (..)
   , SpecState (..)
@@ -36,55 +37,92 @@ import System.IO (hClose)
 fst' :: (a,b,c) -> a
 fst' (a,_,_) = a
 
+-- | Lift a state function into a 'StateT' monad stack
 stateT :: Monad m => (s -> (a, s)) -> StateT s m a
 stateT = StateT . fmap return
 
+-- | Compose two functions, giving 2 inputs to the "first" one.
+-- If 'h = f .: g' then 'h x y = f (g x y)'.
 (.:) :: (a -> b) -> (c -> d -> a) -> c -> d -> b
 (.:) f g x y = f (g x y)
 
-
+-- | Indicate whether to 'Continue' or 'Stop'
+-- with a given state
 data SpecState s = Continue s | Stop s
 
+-- | Continue with a given state
 continue :: Monad m => s -> m (SpecState s)
 continue = return . Continue
 
+-- | Continue (statless)
 continue_ :: Monad m => m (SpecState ())
 continue_ = continue ()
 
+-- | Stop with a given state
 stop :: Monad m => s -> m (SpecState s)
 stop = return . Stop
 
+-- | Stop (stateless)
 stop_ :: Monad m => m (SpecState ())
 stop_ = stop ()
 
+-- | Conditionally continue with a given state,
+-- based on that state and additional given information.
+-- Recommended use: `_loop = \handles -> continueIf f .: runStateT $ do ...`
 continueIf :: Monad m => (a -> s -> Bool) -> m (a,s) -> m (SpecState s)
 continueIf f ms = do
   (a,s) <- ms
   if f a s then continue s else stop s
 
+-- | Conditionally continue statelessly,
+-- based on given information.
+-- Recommended use: '_loop = \handles () -> continueIf_ f $ do ...
+continueIf_ :: Monad m => (a -> Bool) -> m a -> m (SpecState ())
+continueIf_ f ms = continueIf (\a () -> f a) (liftM (\x -> (x,())) ms)
+
+-- | Conditionally continue with a given state,
+-- based solely on that state.
+-- Recommended use: '_loop = \handles -> continueIf' f .: execStateT $ do ..."
 continueIf' :: Monad m => (s -> Bool) -> m s -> m (SpecState s)
 continueIf' f ms = continueIf (\() s -> f s) (liftM ((,) ()) ms)
 
+-- | Conditionally stop with a given state,
+-- based on that state and additional given information.
 stopIf :: Monad m => (a -> s -> Bool) -> m (a,s) -> m (SpecState s)
-stopIf f ms = do
-  (a,s) <- ms
-  if f a s then stop s else continue s
+stopIf f = continueIf (not . f)
 
+-- | Conditionally stop with a given state,
+-- based solely on that state.
 stopIf' :: Monad m => (s -> Bool) -> m s -> m (SpecState s)
-stopIf' f ms = stopIf (\() s -> f s) (liftM ((,) ()) ms)
+stopIf' f = continueIf' (not . f)
+
+-- | Conditionally stop statlessly,
+-- based on given information.
+stopIf_ :: Monad m => (s -> Bool) -> m a -> m (SpecState ())
+stopIf_ f = continueIf_ (not . f)
 
 instance Functor SpecState where
   fmap f (Continue s) = Continue $ f s
   fmap f (Stop s) = Stop $ f s
 
-
-data NetSpec t s = ServerSpec
+-- | Define the specification of your networking task
+-- as a begin, loop, and end proceedure.
+-- 't' indicates the 'Traversable' structure used
+-- ([] is recommended for simplicity, but you are at liberty
+-- to use any 'Traversable' you see fit.
+-- 's' indicates the type used for "state".
+-- Use '()' for a stateless specification.
+data NetSpec t s
+  -- | A server must specify which ports to listen on
+  = ServerSpec
   { _ports  :: t PortID
   , _begin  :: t Handle -> IO s
   , _loop   :: t Handle -> s -> IO (SpecState s)
   , _end    :: t Handle -> s -> IO ()
   }
-                 | ClientSpec
+  
+  -- | A client must specify tuples of (hostname, port) to connect to.
+  | ClientSpec
   { _conns  :: t (String, PortID)
   , _begin  :: t Handle -> IO s
   , _loop   :: t Handle -> s -> IO (SpecState s)
@@ -92,6 +130,17 @@ data NetSpec t s = ServerSpec
   }
 
 
+-- | Run a 'NetSpec'.
+-- Running a spec will step through your 'Traversable'
+-- of connections, and replace each one with a 'Handle',
+-- preserving the structure of the 'Traversable' otherwise.
+-- Regardless of exceptions, all 'Handle's and 'Socket's
+-- opened by the spec will be closed at the end of the run;
+-- you should not need to close any of the 'Handle's given to you
+-- by the spec.
+-- During the run, it is assumed that all connections will
+-- remain open at least until they are no longer needed.
+-- (Note that this calls `withSocketsDo` for you)
 runSpec :: Traversable t => NetSpec t s -> IO ()
 runSpec spec = withSocketsDo $ case spec of
     ServerSpec{} -> bracket a c b
--- src/NetSpec/Text.hs
+++ src/NetSpec/Text.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 
+-- | Use 'Text' to send and receive messages,
+-- delimited by '\n'
 module NetSpec.Text (
     (!)
   , send
@@ -11,7 +13,7 @@ module NetSpec.Text (
   ) where
 
 import System.IO as I (Handle)
-import Data.Text as X (Text)
+import Data.Text as X (Text, pack)
 import Control.Monad.IO.Class as IO (MonadIO, liftIO)
 
 import Data.Text.IO as T
@@ -22,6 +24,10 @@ import System.IO (hFlush)
 infix 2 !
 
 class CanSend h where
+  -- | The staple for sending a message.
+  -- '(!)' is typeclassed so that you can
+  -- 'send' or 'broadcast' using the same simple syntax.
+  -- Usage: 'destinationHandle(s) ! someText'
   (!) :: MonadIO io => h -> Text -> io ()
 
 instance CanSend Handle where
@@ -30,11 +36,14 @@ instance CanSend Handle where
 instance (Foldable f) => CanSend (f Handle) where
   (!) = broadcast
 
+-- | Send a 'Text' message to exactly one 'Handle'.
 send :: MonadIO io => Handle -> Text -> io ()
 send h str = liftIO $ T.hPutStrLn h str >> hFlush h
 
+-- | Broadcast a 'Text' message to multiple 'Handle's.
 broadcast :: MonadIO io => Foldable f => f Handle -> Text -> io ()
 broadcast hs str = F.mapM_ (! str) hs
 
+-- | Receive a 'Text' message from a 'Handle'.
 receive :: MonadIO io => Handle -> io Text
 receive = liftIO . T.hGetLine
